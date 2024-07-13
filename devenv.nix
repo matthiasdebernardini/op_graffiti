f02fd72ac12007c5ecf02133d856207c8705ec40
matthiasdebernardini <m.f.debern@protonmail.com>
2024-07-13 13:42:24 -0500

    init 2

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+	packages = [
+	    pkgs.git
+		pkgs.jq
+		pkgs.just
+		pkgs.openssl
+		pkgs.cargo-nextest
+		pkgs.cargo-watch
+		pkgs.cargo-wizard
+		pkgs.hurl
+		]
+		++
+		lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
+				frameworks.CoreFoundation
+				frameworks.Security
+				frameworks.SystemConfiguration
+		]);
+
+	enterShell = ''
+		cargo --version
+		alias c="cargo"
+		alias cc="cargo check"
+		alias crk="cargo run --bin keel"
+		alias cck="cargo check --bin keel"
+		alias crs="cargo run --bin siren"
+		'';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::restriction)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+
+fn main() {
+    println!("Hello, world!");
+}
