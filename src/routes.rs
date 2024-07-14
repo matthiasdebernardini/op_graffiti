6366850d72ad91b0b1e794cacb73c666689d6926
matthiasdebernardini <m.f.debern@protonmail.com>
2024-07-13 19:35:27 -0500

    chore: docs, error, styling

---
+++ src/routes.rs
@@ -0,0 +1,67 @@
+// External crate imports
+use axum::{extract::Path, response::IntoResponse, Json};
+use bdk_electrum::bdk_chain::bitcoin::script::PushBytesBuf;
+use bdk_wallet::{KeychainKind, SignOptions, Wallet};
+use serde_json::json;
+use tracing::info;
+
+// Local crate imports
+use crate::error::{Graffiti, Report};
+use crate::{
+    error,
+    util::{get_electrum_client, get_tx_details, sync_electrum, NETWORK},
+    EXTERNAL_DESCRIPTOR, INTERNAL_DESCRIPTOR,
+};
+
+#[tracing::instrument]
+pub async fn get_op_return() -> error::Result<impl IntoResponse> {
+    info!("Received READ request for op return transactions");
+    let mut wallet = Wallet::new(EXTERNAL_DESCRIPTOR, INTERNAL_DESCRIPTOR, NETWORK)?;
+
+    sync_electrum(&mut wallet)
+        .await
+        .map_err(|e| Report::from(Graffiti::Anyhow(e)))?;
+
+    let transactions = get_tx_details(&wallet).unwrap();
+
+    let j = json!({ "transactions": transactions });
+
+    Ok(Json(j))
+}
+
+#[tracing::instrument]
+pub async fn write_op_return(Path(data): Path<String>) -> error::Result<impl IntoResponse> {
+    info!("Received WRITE request with data: {}", &data);
+
+    let mut wallet = Wallet::new(EXTERNAL_DESCRIPTOR, INTERNAL_DESCRIPTOR, NETWORK)?;
+    sync_electrum(&mut wallet)
+        .await
+        .map_err(|e| Report::from(Graffiti::Anyhow(e)))?;
+
+    let address = wallet.next_unused_address(KeychainKind::External);
+
+    info!(
+        "Deposit sats to this address in case the wallet is dry: {}",
+        address
+    );
+
+    let mut tx_builder = wallet.build_tx();
+
+    let push_bytes = PushBytesBuf::try_from(data.into_bytes())?;
+    tx_builder.add_data(&push_bytes);
+
+    let mut psbt = tx_builder.finish()?;
+    let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
+
+    assert!(finalized);
+
+    let tx = psbt.extract_tx()?;
+
+    let client = get_electrum_client().map_err(|e| Report::from(Graffiti::Anyhow(e)))?;
+    client.transaction_broadcast(&tx)?;
+
+    let txid = tx.compute_txid();
+    let j = json!({ "txid": txid });
+
+    Ok(Json(j))
+}
--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,23 @@
-use bdk_electrum::electrum_client::Client;
-use bdk_electrum::{electrum_client, BdkElectrumClient};
-use bdk_wallet::bitcoin::Network;
-use bdk_wallet::bitcoin::Network::{Bitcoin, Regtest, Signet, Testnet};
-use bdk_wallet::{KeychainKind, Wallet};
-use std::collections::HashSet;
-use std::io::Write;
+// Third-party crates
+use axum::routing::get;
+use axum::Router;
+use better_panic::Settings;
+use doc_comment::doc_comment;
+use serde::{Serialize, Serializer};
+use serde_json::json;
+use tokio::net::TcpListener;
 use tracing::info;
+
+// BDK (Bitcoin Development Kit) related imports
+use bdk_electrum::bdk_chain::{ChainPosition, ConfirmationTimeHeightAnchor};
+use bdk_electrum::electrum_client::Client;
+use bdk_electrum::BdkElectrumClient;
+use bdk_wallet::bitcoin::script::Instruction;
+use bdk_wallet::bitcoin::Network::{Bitcoin, Regtest, Signet, Testnet};
+use bdk_wallet::bitcoin::{Amount, Network, Txid};
+use bdk_wallet::{floating_rate, Wallet};
+// Local imports
+use crate::routes::{get_op_return, write_op_return};
 
 pub const NETWORK: Network = {
     if cfg!(feature = "bitcoin") {
@@ -21,47 +33,205 @@ pub const NETWORK: Network = {
 const STOP_GAP: usize = 50;
 const BATCH_SIZE: usize = 5;
 
-pub fn sync_electrum(wallet: &mut Wallet) {
-    let client = get_electrum_client();
+#[tracing::instrument]
+pub async fn sync_electrum(wallet: &mut Wallet) -> anyhow::Result<()> {
+    info!("syncing electrum");
+    let client = get_electrum_client()?;
 
     // Populate the electrum client's transaction cache so it doesn't redownload transaction we
     // already have.
     client.populate_tx_cache(&wallet);
 
-    let request = wallet
-        .start_full_scan()
-        .inspect_spks_for_all_keychains({
-            let mut once = HashSet::<KeychainKind>::new();
-            move |k, spk_i, _| {
-                if once.insert(k) {
-                    info!("\nScanning keychain [{k:?}]");
-                } else {
-                    info!(" {spk_i:<3}");
-                }
-            }
-        })
-        .inspect_spks_for_all_keychains(|_, _, _| std::io::stdout().flush().expect("must flush"));
+    let request = wallet.start_full_scan();
 
     // todo might be slow
     let mut update = client
-        .full_scan(request, STOP_GAP, BATCH_SIZE, false)
-        .unwrap()
-        .with_confirmation_time_height_anchor(&client)
-        .unwrap();
+        .full_scan(request, STOP_GAP, BATCH_SIZE, false)?
+        // .unwrap()
+        .with_confirmation_time_height_anchor(&client)?;
+    // .unwrap();
 
     let now = std::time::UNIX_EPOCH.elapsed().unwrap().as_secs();
     let _ = update.graph_update.update_last_seen_unconfirmed(now);
 
-    wallet.apply_update(update).unwrap();
+    wallet.apply_update(update)?;
+    Ok(())
 }
 
-pub fn get_electrum_client() -> BdkElectrumClient<Client> {
+pub fn get_electrum_client() -> anyhow::Result<BdkElectrumClient<Client>> {
     let electrum_url = match NETWORK {
-        // todo point to mempool
         Bitcoin => "ssl://electrum.blockstream.info:50002",
         // "ssl://mempool.space:50002",
         _ => "ssl://mempool.space:60602",
     };
 
-    BdkElectrumClient::new(electrum_client::Client::new(electrum_url).unwrap())
+    let client = Client::new(electrum_url)?;
+    let client = BdkElectrumClient::new(client);
+    Ok(client)
+}
+
+doc_comment!(
+    r#"
+    # Transaction Detail
+
+    `TxDetail` represents the detailed information of a Bitcoin transaction.
+
+    This struct encapsulates various aspects of a transaction, including the amounts
+    involved, the transaction ID, and its position in the blockchain.
+
+    ## Fields
+
+    * `received`: The total amount of Bitcoin received in this transaction.
+    * `sent`: The total amount of Bitcoin sent in this transaction.
+    * `fee`: The transaction fee paid.
+    * `fee_rate`: The fee rate in satoshis per virtual byte (sat/vB).
+    * `txid`: The unique identifier of this transaction.
+    * `chain_position`: The position of this transaction in the blockchain,
+      including confirmation status and block height if confirmed.
+
+    ## Serialization
+
+    This struct derives `Serialize` for easy conversion to various data formats.
+    Note that `chain_position` uses a custom serialization method.
+
+    ## Usage
+
+    `TxDetail` is typically used when querying transaction history or when detailed
+    information about a specific transaction is required.
+
+    ```rust
+    let tx_detail = TxDetail {
+        received: Amount::from_sat(100000),
+        sent: Amount::from_sat(95000),
+        fee: Amount::from_sat(5000),
+        fee_rate: 10.5,
+        txid: Txid::from_str("1234...").unwrap(),
+        chain_position: ChainPosition::Confirmed(ConfirmedAt { height: 700000, time: 1234567890 }),
+    };
+    ```
+    "#,
+    #[derive(serde_derive::Serialize, Clone, Debug)]
+    pub struct TxDetail<'a> {
+        pub received: Amount,
+        pub sent: Amount,
+        pub fee: Amount,
+        pub fee_rate: f64,
+        pub txid: Txid,
+        #[serde(serialize_with = "serialize_chain_position")]
+        pub chain_position: ChainPosition<&'a ConfirmationTimeHeightAnchor>,
+    }
+);
+
+fn serialize_chain_position<S>(
+    chain_position: &ChainPosition<&ConfirmationTimeHeightAnchor>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match chain_position {
+        ChainPosition::Confirmed(anchor) => {
+            let confirmation_time = anchor.confirmation_time;
+            let height = anchor.confirmation_height;
+            let confirmed = json!({
+                "type": "Confirmed",
+                "anchor": {
+                    "height": height,
+                    "confirmation_time": confirmation_time,
+                }
+            });
+            confirmed.serialize(serializer)
+        }
+        ChainPosition::Unconfirmed(ts) => {
+            let unconfirmed = json!({
+                "type": "Unconfirmed",
+                "timestamp": ts,
+            });
+            unconfirmed.serialize(serializer)
+        }
+    }
+}
+
+/// # Errors
+///
+/// Will return errors if there is data missing
+/// fetches details and formats the response
+pub fn get_tx_details(wallet: &Wallet) -> anyhow::Result<Vec<TxDetail>> {
+    wallet
+        .transactions()
+        .filter(|tx| {
+            tx.tx_node.tx.output.iter().any(|output| {
+                output
+                    .script_pubkey
+                    .instructions()
+                    .next()
+                    .map_or(false, |instruction| {
+                        matches!(
+                            instruction,
+                            Ok(Instruction::Op(
+                                bdk_wallet::bitcoin::blockdata::opcodes::all::OP_RETURN
+                            ))
+                        )
+                    })
+            })
+        })
+        .map(|tx| {
+            let txid = tx.tx_node.txid;
+            let chain_position = tx.chain_position;
+            let tx = tx.tx_node.tx.as_ref();
+            let (sent, received) = wallet.sent_and_received(tx);
+            let fee = wallet.calculate_fee(tx)?;
+            let fee_rate = wallet.calculate_fee_rate(tx)?;
+            let fee_rate = floating_rate!(fee_rate);
+
+            let tx_detail = TxDetail {
+                received,
+                sent,
+                fee,
+                fee_rate,
+                txid,
+                chain_position,
+            };
+
+            Ok(tx_detail)
+        })
+        .collect()
+}
+
+pub fn setup_better_panic() {
+    Settings::debug()
+        .most_recent_first(false)
+        .lineno_suffix(true)
+        .install();
+}
+
+pub async fn setup_server() -> anyhow::Result<(Router, TcpListener)> {
+    let app = setup_router();
+
+    let listener = setup_listener().await?;
+    Ok((app, listener))
+}
+
+async fn setup_listener() -> anyhow::Result<TcpListener> {
+    let addr = if cfg!(debug_assertions) {
+        // Bind to localhost (127.0.0.1) for debug builds
+        std::net::SocketAddr::from(([127, 0, 0, 1], 9000))
+    } else {
+        // Bind to all interfaces (0.0.0.0) for release builds
+        std::net::SocketAddr::from(([0, 0, 0, 0], 9000))
+    };
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    Ok(listener)
+}
+
+fn setup_router() -> Router {
+    Router::new()
+        .route("/get_op_return", get(get_op_return))
+        .route("/write_op_return/:data", get(write_op_return))
+}
+
+pub fn setup_tracer() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 }
